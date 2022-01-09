aff13f46f859d5648183def90f4b649919bccedc
prerana1821 <prerananawar1@gmail.com>
2022-01-09 16:15:39 +0530

    add: project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { GlobalHotKeys } from 'react-hotkeys';
+import confetti from 'canvas-confetti';
 
 function App() {
+
+  const colors = ['#F50057', '#61dafb'];
+
+  const keyMap = {
+    test: "c h a n g w o r k s",
+  }
+
+  const handlers = {
+    test: () => {
+      confetti({ particleCount: 150, spread: 180, colors: colors });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <GlobalHotKeys keyMap={ keyMap } handlers={ handlers } />
+      <header className="header">
+        <h1>
+          Type "changworks"
+        </h1>
+        <p>to experience the magic ✨🎊</p>
       </header>
     </div>
   );
